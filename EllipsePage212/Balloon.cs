3aa98355aea3417610a63c0e8874e3a9dc54402b
eline.schepers <eline.schepers@B209-09>
2017-02-06 11:54:20 +0100

    Add project files.

---
+++ EllipsePage212/Balloon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace EllipsePage212
+{
+    public class Balloon
+    {
+        private int x = 50;
+        private int y = 50;
+        private int diameter = 20;
+        private Ellipse ellipse;
+
+        public Balloon()
+        {
+            CreateElipse();
+            UpdateEllipse();
+        }
+        public void MoveRight(int xStep)
+        {
+            x = x + xStep;
+            UpdateEllipse();
+        }
+        public void ChangeSize(int change)
+        {
+            diameter = diameter + change;
+            UpdateEllipse();
+        }
+
+
+        public void DisplayOn(Canvas canvas)
+        {
+            canvas.Children.Add(ellipse);
+        }
+        private void CreateElipse()
+        {
+            ellipse = new Ellipse();
+            ellipse.Stroke = new SolidColorBrush(Colors.Blue);
+            ellipse.StrokeThickness = 2;
+        }
+        private void UpdateEllipse()
+        {
+            ellipse.Margin = new Thickness(x, y, 0, 0);
+            ellipse.Width = diameter;
+            ellipse.Height = diameter;
+        }   
+    }
+}
---
+++ EllipsePage212/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EllipsePage212
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        private Balloon balloon;
+        private Balloon balloon2;
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            balloon = new Balloon();
+            balloon.DisplayOn(canvas);
+
+            balloon2 = new Balloon();
+            balloon2.DisplayOn(canvas);
+        }
+
+        private void Move_Click(object sender, RoutedEventArgs e)
+        {
+            balloon.MoveRight(20);
+            balloon2.MoveRight(20);
+        }
+
+        private void Grow_Click(object sender, RoutedEventArgs e)
+        {
+            balloon.ChangeSize(10);
+            balloon2.ChangeSize(10);
+        }
+    }
+}
